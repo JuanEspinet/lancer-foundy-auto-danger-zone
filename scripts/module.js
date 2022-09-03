3305efcbe0f81bf5f2e2f70113638bd72ed24cea
Juan Espinet <juan.espinet@gmail.com>
2022-09-02 22:24:29 -0700

    initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,24 @@
-Hooks.once('init', async function() {
+function hasEffect(actor, effect) {
+    return actor.data.effects.some(eff => eff.data.flags.core?.statusId?.endsWith(effect));
+}
+  
+function createDangerZoneEffect() {
+    return {
+        id: "dangerzone",
+        label:"Danger Zone",
+        icon:"systems/lancer/assets/icons/white/status_dangerzone.svg"
+    }
+}
 
-});
+let onUpdateToken = (t) => { 
+    let maxHeat = t.actor.data?.data?.derived?.heat?.max;
+    let currHeat = t.actor.data?.data?.derived?.heat?.value;
+    let existingeffect = hasEffect(t.actor,"dangerzone");
 
-Hooks.once('ready', async function() {
+    if ((currHeat >= (maxHeat / 2) && !existingeffect)
+        || (currHeat < (maxHeat / 2) && existingeffect )) {
+        t.toggleActiveEffect(createDangerZoneEffect());
+    }
+};  
 
-});
+Hooks.on('updateToken', onUpdateToken);
